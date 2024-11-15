b5ef630d475325d54eae50095d0b15714b44e04b
Vlad Stirbu <vstirbu@gmail.com>
2024-11-15 11:31:19 +0200

    Add error handling for circuit execution and update VSCode settings; enhance tests for invalid input

--- app/main.py
+++ app/main.py
@@ -1,14 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from qiskit.qasm2.exceptions import QASM2ParseError
 
 from .run import run_circuit
 
 app = FastAPI()
 
+
 class Run(BaseModel):
     circuit: str
 
+
 @app.post("/runs")
 def execute(run: Run):
-    result = run_circuit(run.circuit)
-    return { "status": "COMPLETED", "result": result }
+    try:
+        result = run_circuit(run.circuit)
+        return {"status": "COMPLETED", "result": result}
+    except QASM2ParseError as e:
+        raise HTTPException(
+            status_code=422, detail=[{"type": "value_error", "loc": ["body", "circuit"], "msg": str(e)}]
+        )
--- app/test_main.py
+++ app/test_main.py
@@ -2,9 +2,10 @@ from fastapi.testclient import TestClient
 from qiskit import QuantumCircuit
 from qiskit.qasm2 import dumps
 
-from .main import app
+from app.main import app
 
 client = TestClient(app)
+
 
 def test_run_circuit():
     circuit = QuantumCircuit(2, name="test")
@@ -15,3 +16,11 @@ def test_run_circuit():
     response = client.post("/runs", json={"circuit": dumps(circuit)})
     assert response.status_code == 200
     assert response.json()["status"] == "COMPLETED"
+
+
+def test_run_circuit_invalid():
+    response = client.post("/runs", json={"circuit": "invalid qasm"})
+    assert response.status_code == 422
+    assert response.json()["detail"][0]["msg"] == "\"<input>:1,0: 'invalid' is not defined in this scope\""
+    assert response.json()["detail"][0]["type"] == "value_error"
+    assert response.json()["detail"][0]["loc"] == ["body", "circuit"]
